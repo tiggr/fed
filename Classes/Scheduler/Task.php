deefb815e9362a054583a2b74263016567b6ec31
Claus Due <claus@wildside.dk>
2011-11-26 21:19:59 +0100

    [BUGFIX] Correct CommandController namespace for Scheduler execution
    This fixes the expected namespace when executing CommandController
    commands through Scheduler.
    
    Change-Id: Ide9fa23c8850bab0ba846b7b10932a7ae926bc3c

--- Classes/Scheduler/Task.php
+++ Classes/Scheduler/Task.php
@@ -1,14 +1,32 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 Claus Due, Wildside A/S <claus@wildside.dk>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
 
 /**
- * Description of Task
+ * Scheduler task to execute CommandController commands
  *
- * @author claus
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser Public License, version 3 or later
  */
 class Tx_Fed_Scheduler_Task extends Tx_Scheduler_Task {
 
@@ -50,14 +68,13 @@ class Tx_Fed_Scheduler_Task extends Tx_Scheduler_Task {
 			$controllerObjectNameParts = array(
 				'Tx',
 				Tx_Extbase_Utility_Extension::convertLowerUnderscoreToUpperCamelCase($extensionName),
-				'Controller',
 				'Command',
 				Tx_Extbase_Utility_Extension::convertLowerUnderscoreToUpperCamelCase($controllerName) . 'CommandController'
 			);
 			$controllerObjectName = implode('_', $controllerObjectNameParts);
 			$request->setControllerCommandName($commandName);
 			$request->setControllerObjectName($controllerObjectName);
-			$request->setArguments($this->arguments);
+			$request->setArguments((array) $this->arguments);
 			$dispatcher->dispatch($request, $response);
 			return TRUE;
 		} catch (Exception $e) {
