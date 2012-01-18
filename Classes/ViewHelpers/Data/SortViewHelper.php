93f8e19d4b6b0f6db9458b1dcf347b758f709f44
Claus Due <claus@wildside.dk>
2012-01-18 14:18:57 +0100

    [BUGFIX] Added missing (after Fluid 1.4) ObjetManager, Data/Sort VH
    This simply adds an injection method to set $this->objectManager
    since this no longer is set by Fluid 1.4
    
    Change-Id: I6dbc69f1c5f1000cef4907b124a21db31b62f9cd
    Fixes: #33290

--- Classes/ViewHelpers/Data/SortViewHelper.php
+++ Classes/ViewHelpers/Data/SortViewHelper.php
@@ -35,6 +35,18 @@
 class Tx_Fed_ViewHelpers_Data_SortViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractTagBasedViewHelper {
 
 	/**
+	 * @var Tx_Extbase_Object_ObjectManager
+	 */
+	protected $objectManager;
+
+	/**
+	 * @param Tx_Extbase_Object_ObjectManager $objectManager
+	 */
+	public function injectObjectManager(Tx_Extbase_Object_ObjectManager $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
 	 * Initialize arguments
 	 */
 	public function initializeArguments() {
