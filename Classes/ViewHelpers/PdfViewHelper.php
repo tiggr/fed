5292c3db4b047c8a2b437c00c57825cfda556f14
Claus Due <claus@wildside.dk>
2012-08-19 21:11:28 +0200

    [BUGFIX] Argument passing, PDF ViewHelper
    This change replaces previous argument registration method into a
    list of cliArguments (identical to those used for the wkhtmltopdf
    utility).
    
    Thanks to PowerKiKi for the patch!
    
    Change-Id: I59615b13a425e75d7c200b83642787a84d2fc023
    Fixes: #35344

--- Classes/ViewHelpers/PdfViewHelper.php
+++ Classes/ViewHelpers/PdfViewHelper.php
@@ -32,49 +32,31 @@
 class Tx_Fed_ViewHelpers_PdfViewHelper extends Tx_Fed_Core_ViewHelper_AbstractViewHelper {
 
 	/**
-	 * @var Tx_Fed_Utility_PDF
-	 */
-	protected $pdfService;
-
-	/**
-	 * @author Claus Due, Wildside A/S
-	 * @param Tx_Fed_Utility_PDF $pdfService
-	 * @return void
-	 */
-	public function injectPDF(Tx_Fed_Utility_PDF $pdfService) {
-		$this->pdfService = $pdfService;
-	}
-
-	/**
 	 * Initialize arguments for this ViewHelper
 	 *
 	 * @author Claus Due, Wildside A/S
 	 * @return void
 	 */
 	public function initializeArguments() {
-		$arguments = $this->pdfService->getViewHelperArguments();
-		foreach ($arguments as $name=>$config) {
-			list ($type, $description, $required, $default) = $config;
-			$this->registerArgument($name, $type, $description, $required, $default);
-		}
 		$this->registerArgument('filename', 'string', 'Download as filename', FALSE, 'file.pdf');
 		$this->registerArgument('wkhtmltopdf', 'string', 'Path to executable wkhtmltopdf binary.
 			Defaults to "wkhtmltopdf" which expects the binary to be in the server process\' PATH environment variable', FALSE, 'wkhtmltopdf');
+		$this->registerArgument('cliArguments', 'string', 'Optional arguments to wkhtmltopdf command line.', FALSE, '');
 	}
 
 	/**
-	 * Get an array of the arguments used to implement this instance of the
+	 * Get an array of the arguments values used to implement this instance of the
 	 * ViewHelper.
 	 *
 	 * @author Claus Due, Wildside A/S
 	 * @return array
 	 */
 	protected function getDefinedArguments() {
-		$arguments = $this->pdfService->getViewHelperArguments();
+		$arguments = $this->getAllRegisteredArguments();
 		$defined = array();
-		foreach ($arguments as $name=>$config) {
+		foreach ($arguments as $name => $config) {
 			if ($this->arguments[$name]) {
-				$defined[$name] = $config;
+				$defined[$name] = $this->arguments[$name];
 			}
 		}
 		return $defined;
@@ -89,14 +71,11 @@ class Tx_Fed_ViewHelpers_PdfViewHelper extends Tx_Fed_Core_ViewHelper_AbstractVi
 	public function render() {
 		$uniqId = uniqid('fedPDF_');
 		$extension = 'tx_fed_pdf';
-		$arguments = $this->jsonService->encode($this->getDefinedArguments());
-		$arguments = base64_encode($arguments);
+		$arguments = base64_encode(serialize($this->getDefinedArguments()));
 		$typeNum = 48151623420;
-		$code = <<< CODE
+		$html = <<< CODE
 <form action='?type={$typeNum}' method='post' id='{$uniqId}' style='display: none'>
 <input type='hidden' name='{$extension}[html]' value='' />
-<input type='hidden' name='{$extension}[filename]' value='{$this->arguments['filename']}' />
-<input type='hidden' name='{$extension}[wkhtmltopdf]' value='{$this->arguments['wkhtmltopdf']}' />
 <input type='hidden' name='{$extension}[arguments]' value='{$arguments}' />
 </form>
 CODE;
@@ -109,7 +88,6 @@ function {$uniqId}() {
 SCRIPT;
 		$this->includeHeader($script, 'js');
 		$inner = parent::renderChildren();
-		$html .= $code;
 		$html .= "<a href='javascript:;' class='fed-pdf-link' onclick='{$uniqId}();'>{$inner}</a>";
 		return $html;
 	}
