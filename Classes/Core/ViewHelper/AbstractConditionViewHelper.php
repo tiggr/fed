953ea39fdd71f5bbee87e7be1555771bd1939ccc
Wouter Wolters <typo3@wouterwolters.nl>
2012-10-27 19:44:36 +0200

    [CGL] Fix CGL issues

--- Classes/Core/ViewHelper/AbstractConditionViewHelper.php
+++ Classes/Core/ViewHelper/AbstractConditionViewHelper.php
@@ -1,27 +1,27 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2010 Claus Due <claus@wildside.dk>, Wildside A/S
-*
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2010 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * Provides injected services and methods for easier implementation in
@@ -45,13 +45,13 @@ abstract class Tx_Fed_Core_ViewHelper_AbstractConditionViewHelper extends Tx_Flu
 		if (is_array($haystack)) {
 			$asArray = $haystack;
 		} elseif ($haystack instanceof Tx_Extbase_Persistence_ObjectStorage) {
-			/** @var Tx_Extbase_Persistence_ObjectStorage $haystack */
+			/** @var $haystack Tx_Extbase_Persistence_ObjectStorage */
 			$asArray = $haystack->toArray();
 		} elseif ($haystack instanceof Tx_Extbase_Persistence_LazyObjectStorage) {
-			/** @var Tx_Extbase_Persistence_LazyObjectStorage $haystack */
+			/** @var $haystack Tx_Extbase_Persistence_LazyObjectStorage */
 			$asArray = $haystack->toArray();
 		} elseif ($haystack instanceof Tx_Extbase_Persistence_QueryResult) {
-			/** @var Tx_Extbase_Persistence_QueryResult $haystack */
+			/** @var $haystack Tx_Extbase_Persistence_QueryResult */
 			$asArray = $haystack->toArray();
 		}
 		return $asArray[$index];
@@ -64,11 +64,11 @@ abstract class Tx_Fed_Core_ViewHelper_AbstractConditionViewHelper extends Tx_Flu
 	 */
 	protected function assertHaystackIsQueryResultAndHasNeedle($haystack, $needle) {
 		if ($needle instanceof Tx_Extbase_DomainObject_DomainObjectInterface) {
-			/** @var Tx_Extbase_DomainObject_DomainObjectInterface $needle */
+			/** @var $needle Tx_Extbase_DomainObject_DomainObjectInterface */
 			$needle = $needle->getUid();
 		}
 		foreach ($haystack as $index=>$candidate) {
-			/** @var Tx_Extbase_DomainObject_DomainObjectInterface $candidate */
+			/** @var $candidate Tx_Extbase_DomainObject_DomainObjectInterface */
 			if ($candidate->getUid() == $needle) {
 				return $index;
 			}
@@ -84,7 +84,7 @@ abstract class Tx_Fed_Core_ViewHelper_AbstractConditionViewHelper extends Tx_Flu
 	protected function assertHaystackIsObjectStorageAndHasNeedle($haystack, $needle) {
 		$index = 0;
 		foreach ($haystack as $candidate) {
-			/** @var Tx_Extbase_DomainObject_DomainObjectInterface $candidate */
+			/** @var $candidate Tx_Extbase_DomainObject_DomainObjectInterface */
 			if ($needle instanceof Tx_Extbase_DomainObject_AbstractDomainObject) {
 				$needle = $needle->getUid();
 			}
@@ -105,9 +105,9 @@ abstract class Tx_Fed_Core_ViewHelper_AbstractConditionViewHelper extends Tx_Flu
 		if ($needle instanceof Tx_Extbase_DomainObject_DomainObjectInterface === FALSE) {
 			return array_search($needle, $haystack);
 		} else {
-			/** @var Tx_Extbase_DomainObject_DomainObjectInterface $needle */
+			/** @var $needle Tx_Extbase_DomainObject_DomainObjectInterface */
 			foreach ($haystack as $index=>$straw) {
-				/** @var Tx_Extbase_DomainObject_DomainObjectInterface $straw */
+				/** @var $straw Tx_Extbase_DomainObject_DomainObjectInterface */
 				if ($straw->getUid() == $needle->getUid()) {
 					return $index;
 				}
