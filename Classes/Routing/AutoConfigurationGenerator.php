b7eb6dc24770e0f679bc25c0968fee89ec4baf2d
Claus Due <claus@wildside.dk>
2012-10-28 04:39:18 +0100

    [FEATURE] Realurl AutoConfigurationGenerator
    This will make up the final implementation of the auto-route-builder.
    The hook that post processes rules when automatic Realurl configuration
    is enabled is leveraged to analyze the current site for all occurrences
    of plugins which…
    
    * Use arguments. Without this criteria it makes little sense to make
    any rules. A future revision will allow routing to be triggered using
    an annotation on each controller method, for example to allow pretty
    URLs for Controller/action.
    * Is placed at the top location of all Extbase plugins on the
    particular page where the plugin is inserted. This is the only way
    Realurl is capable of ensuring no colissions happen, but it does still
    allow you to insert multiple plugins that use the same Realurl rule on
    one page - if you are careful about allowing actions and making
    arguments compatible.
    
    This means that the realurl_autoconf.php file must be removed when the
    plugin/page instances change before the URL routes will be rebuilt.
    
    This first version supports lookups of Domain Objects by inspecting the
    TCA of the Model and using the related table name and label field in a
    generic configuration.

--- Classes/Routing/AutoConfigurationGenerator.php
+++ Classes/Routing/AutoConfigurationGenerator.php
@@ -2,10 +2,45 @@
 class Tx_Fed_Routing_AutoConfigurationGenerator {
 
 	/**
-	 * @param array $config
-	 * @param string $extKey
+	 * @var Tx_Flux_Service_Flexform
 	 */
-	public function buildAutomaticRules(&$config, &$extKey) {
+	protected $flexFormService;
+
+	/**
+	 * @var Tx_Extbase_Object_ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var array
+	 */
+	protected $currentTableConfigurationArray = array();
+
+	/**
+	 * @var string
+	 */
+	protected $currentExtensionName = 'Fed';
+
+	/**
+	 * @var array
+	 */
+	protected $excludedArgumentTypes = array(
+		'array'
+	);
+
+	/**
+	 * CONSTRUCTOR
+	 */
+	public function __construct() {
+		$this->objectManager = t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager');
+		$this->flexFormService = $this->objectManager->get('Tx_Flux_Service_Flexform');
+	}
+
+	/**
+	 * @param array $params
+	 * @param object $reference
+	 */
+	public function buildAutomaticRules($params, $reference) {
 		$extensionsAndPluginNames = array();
 		foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions'] as $extensionName => $extensionConfiguration) {
 			foreach ($extensionConfiguration['plugins'] as $pluginName => $pluginConfiguration) {
@@ -14,19 +49,17 @@ class Tx_Fed_Routing_AutoConfigurationGenerator {
 			}
 			unset($extensionConfiguration);
 		}
-		$definitions = $this->buildFixedPostVarSetsForExtensionsAndPluginNames($extensionsAndPluginNames);
-		#header("Content-type: text/plain");
-		#var_dump($definitions);
-		#var_dump($extensionsAndPluginNames);
-		#syslog(LOG_ERR, var_export($extensionsAndPluginNames, TRUE));
-		#syslog(LOG_ERR, var_export($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions'], TRUE));
-		#syslog(LOG_ERR, 'test');
-		#syslog(LOG_ERR, var_export($definitions, TRUE));
-		#exit();
-		$config['fixedPostVars']['_DEFAULT'] = $definitions;
-		#var_dump($config);
-		#exit();
-		return $config;
+		if (isset($params['config']['fixedPostVars']) === FALSE || is_array($params['config']['fixedPostVars']) === FALSE) {
+			$params['config']['fixedPostVars'] = array();
+		}
+		$definitions = $this->buildFixedPostVarsForExtensionsAndPluginNames($extensionsAndPluginNames);
+			// note: foreach-style mapping because array_merge would re-index the numeric
+			// indices which are page UIDs - so this would not suit the purpose of mapping
+		foreach ($definitions as $pidOrName => $definitionOrMappingTarget) {
+			$params['config']['fixedPostVars'][$pidOrName] = $definitionOrMappingTarget;
+		}
+		unset($reference);
+		return $params['config'];
 	}
 
 	/**
@@ -36,14 +69,32 @@ class Tx_Fed_Routing_AutoConfigurationGenerator {
 	 * @param array $extensionsAndPluginNames
 	 * @return void
 	 */
-	protected function buildFixedPostVarSetsForExtensionsAndPluginNames($extensionsAndPluginNames) {
+	protected function buildFixedPostVarsForExtensionsAndPluginNames($extensionsAndPluginNames) {
 		$definitions = array();
+		$pluginSignatures = array();
 		foreach ($extensionsAndPluginNames as $extensionAndPluginName) {
 			list ($extensionName, $pluginName) = explode('->', $extensionAndPluginName);
-			$actions = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions'][$extensionName]['plugins'][$pluginName]['controllers'];
-			foreach ($actions as $controllerName => $controllerConfiguration) {
+			$pluginSignature = strtolower(str_replace('_', '', $extensionName) . '_' . str_replace('_', '', $pluginName));
+			$pluginSignatures[$extensionAndPluginName] = $pluginSignature;
+		}
+		$registeredPluginSignatureValues = array_values($pluginSignatures);
+		foreach ($extensionsAndPluginNames as $extensionAndPluginName) {
+			list ($extensionName, $pluginName) = explode('->', $extensionAndPluginName);
+			$this->currentExtensionName = $extensionName;
+				// Note: these next lines loads a copy of the TCA temporarily
+			$_EXTKEY = t3lib_div::camelCaseToLowerCaseUnderscored($extensionName);
+			$extensionTableConfigurationArrayDefinitionFile = t3lib_extMgm::extPath(strtolower($_EXTKEY), 'ext_tables.php');
+			if (file_exists($extensionTableConfigurationArrayDefinitionFile)) {
+				eval('?>' . file_get_contents($extensionTableConfigurationArrayDefinitionFile));
+				$this->currentTableConfigurationArray = $TCA;
+			} else {
+				$this->currentTableConfigurationArray = array();
+			}
+			$pluginSignature = $pluginSignatures[$extensionAndPluginName];
+			$urlPrefix = 'tx_' . $pluginSignature;
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions'][$extensionName]['plugins'][$pluginName]['controllers'] as $controllerName => $controllerConfiguration) {
 				foreach ($controllerConfiguration['actions'] as $actionName) {
-					$identity = $extensionName . '_' . $pluginName . '_' . $controllerName . '_' . $actionName;
+					$identity = $pluginSignature . '_' . $controllerName . '_' . $actionName;
 					$controllerClassName = 'Tx_' . $extensionName . '_Controller_' . $controllerName . 'Controller';
 					$controllerClassReflection = new ReflectionClass($controllerClassName);
 					if (method_exists($controllerClassName, $actionName . 'Action') === FALSE) {
@@ -51,17 +102,99 @@ class Tx_Fed_Routing_AutoConfigurationGenerator {
 					}
 					$methodReflection = $controllerClassReflection->getMethod($actionName . 'Action');
 					$arguments = $methodReflection->getParameters();
-					$urlPrefix = 'tx_' . strtolower(str_replace('_', '', $extensionName) . '_' . str_replace('_', '', $pluginName));
-					$fixedPostVarSets = array();
-					foreach ($arguments as $argumentReflection) {
-						array_push($fixedPostVarSets, $this->buildFixedPostVarSetForControllerActionArgument($argumentReflection, $actionName, $urlPrefix));
+					if (count($arguments) === 0) {
+						continue;
 					}
-					#$pageUids = $this->getAllPAgeUidsWherePluginOccursFirstInColPosZero()
-					$definitions[$identity] = $fixedPostVarSets;
+					$definitions[$identity] = array(
+						$this->buildFixedPostVarsForController($urlPrefix),
+						$this->buildFixedPostVarsForControllerAction($urlPrefix),
+					);
+					foreach ($arguments as $argumentReflection) {
+						$segment = $this->buildFixedPostVarsForControllerActionArgument($argumentReflection, $actionName, $urlPrefix);
+						if ($segment !== NULL) {
+							array_push($definitions[$identity], $segment);
+						}
+					}
+					$pageUids = $this->getAllPAgeUidsWithPluginSignatureInColPosZeroTop($extensionName, $pluginName, $controllerName, $actionName, $registeredPluginSignatureValues);
+					foreach ($pageUids as $pid) {
+						$definitions[$pid] = $identity;
+					}
 				}
 			}
 		}
 		return $definitions;
+	}
+
+	/**
+	 * Get an array of UIDs of all pages on which $pluginSignature is the first
+	 * Extbase plugin in colPos zero.
+	 *
+	 * @param string $extensionName
+	 * @param string $pluginName
+	 * @param string $controllerName
+	 * @param string $actionName
+	 * @param array $registeredPluginSignatures
+	 * @return array
+	 */
+	protected function getAllPAgeUidsWithPluginSignatureInColPosZeroTop($extensionName, $pluginName, $controllerName, $actionName, $registeredExtbasePluginSignatures) {
+		$pluginSignature = strtolower(str_replace('_', '', $extensionName) . '_' . str_replace('_', '', $pluginName));
+		$clause = "t.deleted = '0' AND t.hidden = '0' AND t.starttime <= '" . time() . "' AND (t.endtime >= '" . time() . "' OR t.endtime = '0') AND t.colPos = '0' AND p.deleted = '0' AND p.uid = t.pid";
+		$orderedPageSignatures = array();
+		$contentRecords = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('t.pid, t.CType, t.list_type, t.pi_flexform', 'tt_content t, pages p', $clause, 'p.pid, t.sorting DESC');
+		$pageUids = array();
+		foreach ($contentRecords as $contentRecord) {
+			$pid = $contentRecord['pid'];
+			$signature = $contentRecord['list_type'] ? $contentRecord['list_type'] : $contentRecord['CType'];
+			if (in_array($signature, $registeredExtbasePluginSignatures)) {
+				$orderedPageSignatures[$pid] = array(
+					$signature,
+					$contentRecord['pi_flexform']
+				);
+			}
+		}
+		foreach ($orderedPageSignatures as $pid => $signatureAndFlexform) {
+			list ($signature, $flexFormSource) = $signatureAndFlexform;
+			if ($flexFormSource) {
+				$decoded = $this->flexFormService->convertFlexFormContentToArray($flexFormSource);
+			} else {
+				$decoded = NULL;
+			}
+			if (isset($decoded['switchableControllerActions'])) {
+				list ($contentRecordController, $contentRecordControllerAction) = explode('->', $decoded['switchableControllerActions']);
+			} else {
+				reset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions'][$extensionName]['plugins'][$pluginName]['controllers']);
+				$contentRecordController = key($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions'][$extensionName]['plugins'][$pluginName]['controllers']);
+				$contentRecordControllerAction = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions'][$extensionName]['plugins'][$pluginName]['controllers'][$contentRecordController]['actions'][0];
+			}
+			if ($signature === $pluginSignature && $contentRecordController === $controllerName && $contentRecordControllerAction === $actionName) {
+				array_push($pageUids, $pid);
+			}
+		}
+		return $pageUids;
+	}
+
+	/**
+	 * @param string $urlPrefix
+	 * @return array
+	 */
+	protected function buildFixedPostVarsForController($urlPrefix) {
+		$definition = array(
+			'GETvar' => $urlPrefix . '[controller]',
+			'noMatch' => 'bypass'
+		);
+		return $definition;
+	}
+
+	/**
+	 * @param string $urlPrefix
+	 * @return array
+	 */
+	protected function buildFixedPostVarsForControllerAction($urlPrefix) {
+		$definition = array(
+			'GETvar' => $urlPrefix . '[action]',
+			'noMatch' => 'bypass'
+		);
+		return $definition;
 	}
 
 	/**
@@ -70,13 +203,47 @@ class Tx_Fed_Routing_AutoConfigurationGenerator {
 	 * @param string $urlPrefix
 	 * @return array
 	 */
-	protected function buildFixedPostVarSetForControllerActionArgument(ReflectionParameter $argument, $actionName, $urlPrefix) {
+	protected function buildFixedPostVarsForControllerActionArgument(ReflectionParameter $argument, $actionName, $urlPrefix) {
 		$argumentName = $argument->getName();
 		$definition = array(
 			'GETvar' => $urlPrefix . '[' . $argumentName . ']',
 		);
-		if ($argument->isDefaultValueAvailable()) {
-			$definition['noMatch'] = 'bypass';
+		if ($argument->allowsNull()) {
+			$definition['noMatch'] = 'null';
+		}
+		$docComment = $argument->getDeclaringFunction()->getDocComment();
+		$matches = array();
+		preg_match('/@param[\s]+([a-zA-Z_0-9\\^\s]+)[\s]+\$' . $argumentName . '/', $docComment, $matches);
+		$argumentDataType = trim($matches[1]);
+		if (in_array($argumentDataType, $this->excludedArgumentTypes) === TRUE) {
+			return NULL;
+		} elseif (class_exists($argumentDataType) && in_array('Tx_Extbase_DomainObject_DomainObjectInterface', class_implements($argumentDataType))) {
+			$tableName = strtolower($argumentDataType);
+			if (isset($this->currentTableConfigurationArray[$tableName]) === TRUE) {
+					// Note: unsetting here since we otherwise would not transform the argument using lookup
+				unset($definition['noMatch']);
+				$_EXTKEY = t3lib_div::camelCaseToLowerCaseUnderscored($this->currentExtensionName);
+				$TCA[$tableName] = $this->currentTableConfigurationArray;
+				$extensionConfigurationFile = t3lib_extMgm::extPath($_EXTKEY, 'ext_tables.php');
+				if (file_exists($extensionConfigurationFile)) {
+					eval('?>' . file_get_contents($extensionConfigurationFile));
+				}
+				$dynamicConfigurationFile = $TCA[$tableName]['ctrl']['dynamicConfigFile'];
+				if (file_exists($dynamicConfigurationFile)) {
+					eval('?>' . file_get_contents($dynamicConfigurationFile));
+				}
+				$definition['lookUpTable'] = array(
+					'table' => $tableName,
+					'id_field' => 'uid',
+					'alias_field' => $TCA[$tableName]['ctrl']['label'],
+					'addWhereClause' => ' AND NOT deleted',
+					'useUniqueCache' => 1,
+					'useUniqueCache_conf' => array(
+						'strtolower' => 1,
+						'spaceCharacter' => '-'
+					)
+				);
+			}
 		}
 		return $definition;
 	}
