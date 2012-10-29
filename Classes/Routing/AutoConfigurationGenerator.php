85984157fe4a9fe63d49e58ae857300133118b0b
Claus Due <claus@wildside.dk>
2012-10-29 21:17:46 +0100

    [TASK] Adapt AutoConfigurationGenerator to SegmentValueProcessor

--- Classes/Routing/AutoConfigurationGenerator.php
+++ Classes/Routing/AutoConfigurationGenerator.php
@@ -320,115 +320,42 @@ class Tx_Fed_Routing_AutoConfigurationGenerator {
 			'GETvar' => $urlPrefix . '[' . $argumentName . ']',
 		);
 		$noMatchRule = $this->assertNoMatchRule($annotations, $argumentName);
-		$definition['noMatch'] = $noMatchRule;
 
 		$docComment = $argument->getDeclaringFunction()->getDocComment();
 		$matches = array();
 		preg_match('/@param[\s]+([a-zA-Z_0-9\\^\s]+)[\s]+\$' . $argumentName . '/', $docComment, $matches);
 		$argumentDataType = trim($matches[1]);
+		/*
 		if (in_array($argumentDataType, $this->excludedArgumentTypes) === TRUE) {
 			return NULL;
 		}
-		if (class_exists($argumentDataType) && in_array('Tx_Extbase_DomainObject_DomainObjectInterface', class_implements($argumentDataType))) {
-			$tableName = strtolower($argumentDataType);
-			if (isset($this->currentTableConfigurationArray[$tableName]) === TRUE) {
-				$_EXTKEY = t3lib_div::camelCaseToLowerCaseUnderscored($this->currentExtensionName);
-				$TCA[$tableName] = $this->currentTableConfigurationArray;
-				$extensionConfigurationFile = t3lib_extMgm::extPath($_EXTKEY, 'ext_tables.php');
-				if (file_exists($extensionConfigurationFile)) {
-					eval('?>' . file_get_contents($extensionConfigurationFile));
-				}
-				$dynamicConfigurationFile = $TCA[$tableName]['ctrl']['dynamicConfigFile'];
-				if (file_exists($dynamicConfigurationFile)) {
-					eval('?>' . file_get_contents($dynamicConfigurationFile));
-				}
-				$definition['lookUpTable'] = array(
-					'table' => $tableName,
-					'id_field' => 'uid',
-					'alias_field' => $TCA[$tableName]['ctrl']['label'],
-					'addWhereClause' => ' AND NOT deleted',
-					'useUniqueCache' => 1,
-					'useUniqueCache_conf' => array(
-						'strtolower' => 1,
-						'spaceCharacter' => '-'
-					)
-				);
-			}
-			#var_dump($definition['noMatch']);
+		$recognizedDataTypes = array('boolean', 'string', 'integer', 'float', '');
+		if (in_array($argumentDataType, $recognizedDataTypes) === TRUE) {
+			return $definition;
+		} elseif (class_exists($argumentDataType) === FALSE) {
+			throw new Tx_Fed_Routing_RoutingException('The class requested in the type for ' . $argumentName . ' (type is ' . $argumentDataType . ') could not be loaded / does not exist', 1351533910);
+		} elseif (in_array('Tx_Extbase_DomainObject_DomainObjectInterface', class_implements($argumentDataType)) === FALSE) {
+			throw new Tx_Fed_Routing_RoutingException('The class ' . $argumentDataType . ' does not implement Tx_Extbase_DomainObject_DomainObjectInterface - not a Domain Object?', 1351533986);
+		}
+		*/
+		$tableName = strtolower($argumentDataType);
+		switch ($argumentDataType) {
+			case '': $conversionMethod = Tx_Fed_Routing_SegmentValueProcessor::CONVERT_NULL; break;
+			case 'DateTime': $conversionMethod = Tx_Fed_Routing_SegmentValueProcessor::CONVERT_DATETIME; break;
+			default: $conversionMethod = Tx_Fed_Routing_SegmentValueProcessor::CONVERT_MODEL; break;
+		}
+
+		$definition['userFunc'] = 'Tx_Fed_Routing_SegmentValueProcessor->translateSegmentValue';
+		$definition['parameters'] = array(
+			'conversionMethod' => $conversionMethod,
+			'className' => $argumentDataType,
+			'tableName' => $tableName
+		);
+		if ($noMatchRule !== NULL) {
+			$definition['parameters']['noMatch'] = $noMatchRule;
+			unset($definition['noMatch']);
 		}
 		return $definition;
-	}
-
-	/**
-	 * @param array $contentRecord
-	 * @return array|NULL
-	 */
-	protected function getSetupForRecord($contentRecord) {
-		$typoScriptDefinition = $GLOBALS['TSFE']->tmpl->setup['tt_content.'];
-		if ($contentRecord['list_type'] && isset($typoScriptDefinition['list.']['20.'][$contentRecord['list_type'] . '.'])) {
-			$setup = $typoScriptDefinition['list.']['20.'][$contentRecord['list_type'] . '.'];
-		} elseif ($contentRecord['CType']) {
-			$setup = $typoScriptDefinition[$contentRecord['CType'] . '.'];
-		} else {
-			$setup = NULL;
-		}
-		return $setup;
-	}
-
-	/**
-	 * Asserts wether or not a content record renders an Extbase plugin
-	 *
-	 * @param array $contentRecord
-	 * @param array|NULL $setup
-	 * @return boolean
-	 */
-	protected function assertIsExtbasePlugin($contentRecord, $setup = NULL) {
-		if ($setup === NULL) {
-			$setup = $this->getSetupForRecord($contentRecord);
-		}
-		if ($setup === NULL) {
-			return FALSE;
-		}
-		if ($this->assertArrayContainsValueRecursive($setup, $this->extbaseUserFunctionIdentifiers)) {
-			return TRUE;
-		}
-		return FALSE;
-	}
-
-	/**
-	 * @param array $array
-	 * @param mixed $value
-	 * @return mixed
-	 */
-	protected function getArrayValueRecursive(array $array, $value) {
-		foreach ($array as $key => $member) {
-			if (is_array($member) && $this->getArrayValueRecursive($member, $value)) {
-				return $member;
-			} elseif (is_array($value) && in_array($member, $value)) {
-				return $member;
-			} elseif ($key == $value) {
-				return $member;
-			}
-		}
-		return NULL;
-	}
-
-	/**
-	 * @param array $array
-	 * @param mixed $value
-	 * @return boolean
-	 */
-	protected function assertArrayContainsValueRecursive(array $array, $value) {
-		foreach ($array as $member) {
-			if (is_array($member) && $this->assertArrayContainsValueRecursive($member, $value)) {
-				return $member;
-			} elseif (is_array($value) && in_array($member, $value)) {
-				return $member;
-			} elseif ($member == $value) {
-				return $member;
-			}
-		}
-		return FALSE;
 	}
 
 }
