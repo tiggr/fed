d122dff4ff98a1413c4c5d5d27ee5d1523494907
Claus Due <claus@wildside.dk>
2013-02-17 21:09:59 +0100

    [TASK] Don't add new content wizard items for FE plugins

--- ext_localconf.php
+++ ext_localconf.php
@@ -84,13 +84,6 @@ if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup']['enableSolrFeatures']
 }
 
 if ($loadBackendConfiguration) {
-	if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup']['enableSolrFeatures']
-	|| $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fed']['setup']['enableFrontendPlugins']
-	) {
-		array_push($fedWizardElements, 'template');
-		array_push($fedWizardElements, 'datasource');
-		array_push($fedWizardElements, 'solr');
-	}
 
 	Tx_Fed_Core::loadRegisteredFluidContentElementTypoScript();
 
