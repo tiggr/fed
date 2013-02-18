57ac01ed08e5ff01325008c69b49c2b21cab7e72
Claus Due <claus@wildside.dk>
2013-02-18 11:37:01 +0100

    [TASK] Use jQuery bind() instead of live() in FileListEditor.js

--- Resources/Public/Javascript/FileListEditor.js
+++ Resources/Public/Javascript/FileListEditor.js
@@ -56,7 +56,7 @@
 			};
 			tableHeader.attr('id', options.editorId);
 			tableHeader.find('.plupload_filelist_header').append('<td class="plupload_cell plupload_file_delete"></td>');
-			jQuery(this).find('.remove').live('click', function() {
+			jQuery(this).find('.remove').bind('click', function() {
 				var row = jQuery(this).parents('tr:first');
 				var filename = row.find('.plupload_file_name span').html().trim();
 				if (filename.length < 1) {
